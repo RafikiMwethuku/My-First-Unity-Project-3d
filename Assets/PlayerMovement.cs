4a6266e1154ab92822ce72caa664454c765fdb96
RafikiMwethuku <Rmwathuku@gmail.com>
2025-07-07 19:09:38 -0400

    Revamped Movement Script and Obstacles added
    Revamped the player movement script to allow for smoother movements using the 'a' and 'd' inputs from the player (velocity based force change instead of default force change). There is a constant forward force now. Also added obstacles in the player's path. Upon hitting one of the obstacles, a message is printed to the console and the player's movement stops. Finally, added fog to limit player's visibiliy of future obstacles

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,21 +7,24 @@ public class PlayerMovement : MonoBehaviour
 
     public Rigidbody rb;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
 
-        //rigidBody = rigidBody ? rigidBody : GetComponent<Rigidbody>();
-        
-        rb.useGravity = false;
-
-        
-
-    }
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate() //use FixedUpdate for physics stuff
     {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
         
     }
 }
